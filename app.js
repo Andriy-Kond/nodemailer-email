b7052d8289e3881381b05e535f208bcb0e7c4de9
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-06-13 11:48:55 +0300

    INIT

---
+++ app.js
@@ -0,0 +1,23 @@
+const sgMail = require('@sendgrid/mail');
+require('dotenv').config();
+const sendEmail = require('./helpers/sendEmail');
+
+const { SENDGRID_REST_API_KEY } = process.env;
+
+// Передаємо ключ у імпортований об'єкт sgMail
+sgMail.setApiKey(SENDGRID_REST_API_KEY);
+// Після цього об'єкт sgMail готовий відправляти email.
+// Але перед тим як відправляти його треба створити:
+const email = {
+  to: 'molobe5202@ratedane.com', // Кому ми відправляємо пошту
+  from: 'akwebua.study@gmail.com', // Від кого
+  subject: 'Test email', // Тема листа
+  html: '<p><strong>Test email</strong> from localhost:3000!!! </p>', // Зміст листа
+};
+// Більшість css-правил поштові клієнти не підтримують. Тому тре використовувати шаблонізатори по типу npm i ejs
+
+// Щоби відправити email потрібно викликати метод send(), який повертає проміс:
+sgMail
+  .send(email)
+  .then(() => console.log('Email send success'))
+  .catch(error => console.log(error.message));
