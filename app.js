28e1de18e04d65869f3a6e4e8d0b3a4b2d37f8ce
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-06-13 12:12:49 +0300

    ADD: nodemailer fn

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-const sgMail = require('@sendgrid/mail');
 require('dotenv').config();
-const sendEmail = require('./helpers/sendEmail');
+// 1. імпортуємо nodemailer
+const nodemailer = require('nodemailer');
 
-const { SENDGRID_REST_API_KEY } = process.env;
+const { META_PASS } = process.env;
 
-// Передаємо ключ у імпортований об'єкт sgMail
-sgMail.setApiKey(SENDGRID_REST_API_KEY);
-// Після цього об'єкт sgMail готовий відправляти email.
-// Але перед тим як відправляти його треба створити:
+// 2. Створюємо об'єкт налаштувань:
+const nodemailerConfig = {
+  host: 'smtp.meta.ua', // Адреса поштового сервера
+  port: 465, // Порт на поштовому сервері, до якого треба підключитись. Ще може бути 25, 465 і 2525, але вони не захищені на відміну від 465
+  secure: true, // Чи треба шифрувати трафік. На 465 порті треба.
+  auth: {
+    user: 'mycloud@meta.ua', // Пошта, до якої ми підключаємось - цю пошту треба шукати на сервері, що прописаний у host: 'smtp.meta.ua'
+    pass: META_PASS, // пароль від цієї пошти
+  },
+};
+
+// 3. Створюємо транспорт
+// Щоб відправити листа, треба за допомогою цього nodemailerConfig створити об'єкт транспорт (налаштований nodemailer), який буде займатись відправленням пошти
+const transport = nodemailer.createTransport(nodemailerConfig);
+
+// 4. Створюємо email
 const email = {
   to: 'molobe5202@ratedane.com', // Кому ми відправляємо пошту
-  from: 'akwebua.study@gmail.com', // Від кого
+  from: 'mycloud@meta.ua', // Від кого
   subject: 'Test email', // Тема листа
   html: '<p><strong>Test email</strong> from localhost:3000!!! </p>', // Зміст листа
 };
-// Більшість css-правил поштові клієнти не підтримують. Тому тре використовувати шаблонізатори по типу npm i ejs
 
-// Щоби відправити email потрібно викликати метод send(), який повертає проміс:
-sgMail
-  .send(email)
+// 5. Відправляємо пошту:
+transport
+  .sendMail(email)
   .then(() => console.log('Email send success'))
   .catch(error => console.log(error.message));
